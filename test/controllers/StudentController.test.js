ea35c6a7a44f42ba8ae8e4b0ebffefb179d3b862
Alejandro Gutiérrez <alexgtz.up@gmail.com>
2022-04-30 00:41:24 -0500

    Corrigiendo estilo con linter

--- test/controllers/StudentController.test.js
+++ test/controllers/StudentController.test.js
@@ -1,38 +1,38 @@
-const StudentService = require('../../lib/services/StudentService');
-const StudentController = require('../../lib/controllers/StudentController');
+const StudentService = require("../../lib/services/StudentService");
+const StudentController = require("../../lib/controllers/StudentController");
 
 describe("Tests para Student Controller", () => {
     test("Obtener estudiantes", () => {
-        const students = StudentController.getStudents('test_file');
+        const students = StudentController.getStudents("test_file");
         expect(students).toStrictEqual([
             {
-              "id": "6264d5d89f1df827eb84bb23",
-              "name": "Warren",
-              "email": "Todd@visualpartnership.xyz",
-              "credits": 200,
-              "enrollments": [
-                "Visual Thinking Intermedio",
-                "Visual Thinking Avanzado"
-              ],
-              "previousCourses": 1,
-              "haveCertification": true
+                "id": "6264d5d89f1df827eb84bb23",
+                "name": "Warren",
+                "email": "Todd@visualpartnership.xyz",
+                "credits": 200,
+                "enrollments": [
+                    "Visual Thinking Intermedio",
+                    "Visual Thinking Avanzado"
+                ],
+                "previousCourses": 1,
+                "haveCertification": true
             },
             {
-              "id": "6264d5d85cf81c496446b67f",
-              "name": "Lucinda",
-              "email": "Sexton@visualpartnership.xyz",
-              "credits": 677,
-              "enrollments": [
-                "Visual Thinking Avanzado"
-              ],
-              "previousCourses": 6,
-              "haveCertification": false
+                "id": "6264d5d85cf81c496446b67f",
+                "name": "Lucinda",
+                "email": "Sexton@visualpartnership.xyz",
+                "credits": 677,
+                "enrollments": [
+                    "Visual Thinking Avanzado"
+                ],
+                "previousCourses": 6,
+                "haveCertification": false
             }
-        ])
+        ]);
     });
     test("Obtener email de estudiantes con certificado", () => {
         const studentEmailsWC = StudentController.getStudentEmailsWC("test_file");
-        expect(studentEmailsWC).toStrictEqual(['Todd@visualpartnership.xyz']);
+        expect(studentEmailsWC).toStrictEqual(["Todd@visualpartnership.xyz"]);
     });
     test("Obtener estudiantes con créditos mayores a 500", () => {
         const studentsOver500c = StudentController.getStudentsOver500c("test_file");
@@ -43,10 +43,10 @@ describe("Tests para Student Controller", () => {
                 "email": "Sexton@visualpartnership.xyz",
                 "credits": 677,
                 "enrollments": [
-                  "Visual Thinking Avanzado"
+                    "Visual Thinking Avanzado"
                 ],
                 "previousCourses": 6,
                 "haveCertification": false
-              }]);
+            }]);
     });
-})
+});
--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -1,27 +1,27 @@
-const Reader = require('../../lib/utils/Reader')
-const StudentService = require('../../lib/services/StudentService')
+const Reader = require("../../lib/utils/Reader");
+const StudentService = require("../../lib/services/StudentService");
 
 describe("Tests para Student Service", () => {
     test("Filtro por certificado", () => {
         const studentsWithCertificate = StudentService.filterByCertification("test_file.json");
         expect(studentsWithCertificate).toStrictEqual([
             {
-              "id": "6264d5d89f1df827eb84bb23",
-              "name": "Warren",
-              "email": "Todd@visualpartnership.xyz",
-              "credits": 200,
-              "enrollments": [
-                "Visual Thinking Intermedio",
-                "Visual Thinking Avanzado"
-              ],
-              "previousCourses": 1,
-              "haveCertification": true
+                "id": "6264d5d89f1df827eb84bb23",
+                "name": "Warren",
+                "email": "Todd@visualpartnership.xyz",
+                "credits": 200,
+                "enrollments": [
+                    "Visual Thinking Intermedio",
+                    "Visual Thinking Avanzado"
+                ],
+                "previousCourses": 1,
+                "haveCertification": true
             }
-        ])
+        ]);
     });
     test("Obtener email de estudiantes con certificado", () => {
         const studentEmailsWC = StudentService.getEmails("test_file.json");
-        expect(studentEmailsWC).toStrictEqual(['Todd@visualpartnership.xyz']);
+        expect(studentEmailsWC).toStrictEqual(["Todd@visualpartnership.xyz"]);
     });
     test("Obtener estudiantes con créditos mayores a 500", () => {
         const studentsOver500c = StudentService.getStudentsOver500c("test_file.json");
@@ -32,10 +32,10 @@ describe("Tests para Student Service", () => {
                 "email": "Sexton@visualpartnership.xyz",
                 "credits": 677,
                 "enrollments": [
-                  "Visual Thinking Avanzado"
+                    "Visual Thinking Avanzado"
                 ],
                 "previousCourses": 6,
                 "haveCertification": false
-              }]);
+            }]);
     });
-})
+});
