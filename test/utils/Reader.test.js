4b3d7c9040576cc96e6c302b65c2bf86db0a83b0
Alejandro Gutiérrez <alexgtz.up@gmail.com>
2022-04-29 22:52:30 -0500

    Agregando Reader con su respectiva prueba de unidad

---
+++ test/utils/Reader.test.js
@@ -0,0 +1,32 @@
+const Reader = require('../../lib/utils/Reader')
+
+describe("Tests para Reader", () => {
+    test("Leyendo archivo JSON", () => {
+        const students = Reader.readJsonFile("test/test_file.json")
+        expect(students).toStrictEqual([
+            {
+              "id": "6264d5d89f1df827eb84bb23",
+              "name": "Warren",
+              "email": "Todd@visualpartnership.xyz",
+              "credits": 508,
+              "enrollments": [
+                "Visual Thinking Intermedio",
+                "Visual Thinking Avanzado"
+              ],
+              "previousCourses": 1,
+              "haveCertification": true
+            },
+            {
+              "id": "6264d5d85cf81c496446b67f",
+              "name": "Lucinda",
+              "email": "Sexton@visualpartnership.xyz",
+              "credits": 677,
+              "enrollments": [
+                "Visual Thinking Avanzado"
+              ],
+              "previousCourses": 6,
+              "haveCertification": true
+            }
+        ])
+    })
+})
